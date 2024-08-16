dbdc3ae78c733a76e6fca83895c436994c38d3e6
Dekan <dekanbrown@odyssy.io>
2024-08-16 12:24:10 -0600

    nft-escrow

---
+++ contracts/nftEscrow/IYeetNftEscrowShaman.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.7 <0.9.0;
+
+interface IYeetNftEscrowShaman {
+    function execute() external;
+
+    function executed() external view returns (bool);
+
+    function threshold() external view returns (uint256);
+
+    function expiration() external view returns (uint256);
+
+    function seller() external view returns (address);
+
+    function nftAddress() external view returns (address);
+
+    function tokenId() external view returns (uint256);
+
+    function yeethBalance() external view returns (uint256);
+}
